eefe8712fae45c5ec6662134d4f467243593ab56
jrozal <68182930+jrozal@users.noreply.github.com>
2021-08-15 21:43:09 -0700

    Add comments to scrape and parse function

--- src/lib/scrapeAndConvert.ts
+++ src/lib/scrapeAndConvert.ts
@@ -7,6 +7,7 @@ interface DataValues {
   'Location served': string;
 };
 
+// function scrapeAndConvertData scrapes table data one row at a time, coverts string data, and returns results as an array of objects
 export async function scrapeAndConvertData(page: puppeteer.Page) {
   try {
     return await page.evaluate(() => {
@@ -14,8 +15,10 @@ export async function scrapeAndConvertData(page: puppeteer.Page) {
 
       const convertData = (obj: DataValues): DataValues => {
         return ({
+          // substitue all numbers in table data to the string 'DAPI'
           'iata': obj['iata'].replace(/[0-9]/g, 'DAPI'),
           'icao': obj['icao'].replace(/[0-9]/g, 'DAPI'),
+          // convert 'airport name' and 'Location served' to lower case, replace whitespaces and commas with '_' underscore
           'airport name': obj['airport name'].replace(/[0-9]/g, 'DAPI').toLowerCase().replace(/[\s,]+/g,'_').trim(),
           'Location served': obj['Location served'].replace(/[0-9]/g, 'DAPI').toLowerCase().replace(/[\s,]+/g,'_').trim()
         });
